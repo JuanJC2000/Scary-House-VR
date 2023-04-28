a84f004b602b17b1da8a4ce8a0789a063a6458eb
jj246324 <jj246324@falmouth.ac.uk>
2023-04-29 00:24:03 +0100

    Added new projectile motion and renamed scene
    Added new projectile motion script which instanties a prefab and adds a force it

--- Scary House/Assets/RaycastShooting.cs
+++ Scary House/Assets/RaycastShooting.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RaycastShooting : MonoBehaviour
 {
     public Transform outRay;
+    [SerializeField] private float shotPower = 500f;
+    [SerializeField] private Transform bulletLeave;
+    public GameObject bulletPrefab;
+
+    public Rigidbody rb;
+    
     
 
     // Start is called before the first frame update
@@ -33,6 +40,23 @@ public class RaycastShooting : MonoBehaviour
                 Destroy(hit.transform.gameObject);
             }
         }
+    }
+
+    public void Shoot()
+    {
+       // Instantiate(bulletPrefab, bulletLeave.position, bulletLeave.rotation).GetComponent<Rigidbody>().AddForce(bulletLeave.forward * shotPower);
+        var varName = Instantiate(bulletPrefab, bulletLeave.position, bulletLeave.rotation);
+
+        Rigidbody rb = varName.GetComponent<Rigidbody>();
+       // varName2.WakeUp();
+        rb.AddForce(rb.transform.forward * shotPower);
+        Debug.Log("I have shot" + rb.transform.forward * shotPower );
+        
+
+
+
+
+
     }
 
 
